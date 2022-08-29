c7ba68a8947a1f26d3526c415cabedab0fd3e6f2
William Touboul <Touboul.william@gmail.com>
2022-08-29 16:03:52 +0200

    animation²

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -1,13 +1,36 @@
 var tl = gsap.timeline();
 tl.from('#name_top_left', {
     y: 150
-});
+}, 1);
 
-tl.from('.h_top_left', {
+tl.from('.slash', {
+    x: -100,
+    stagger: 0.3
+
+}, 2);
+
+tl.to('.second_slash', {
+    x: '38vw',
+    duration: 1.4,
+    stagger: 0.4
+}, 3);
+
+
+tl.from('.inner_title', {
     x: '-50vw',
-    stagger: 0.5,
+    duration: 1.4,
+    stagger: 0.8
+}, 3);
+
+tl.to('.second_slash', {
+    delay: 1.4,
+    x: -10
+}, 4);
+
+tl.from('.h3_top_left', {
+    x: '-38vw',
     duration: 1.4
-});
+})
 
 tl.from('#square_portfolio', {
     x: '25vw',
