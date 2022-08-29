512581894dd5d1d20c7f4ee93bdcdfdd0e400c04
William Touboul <Touboul.william@gmail.com>
2022-08-29 16:10:04 +0200

    reparation anim

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -1,36 +1,13 @@
 var tl = gsap.timeline();
 tl.from('#name_top_left', {
     y: 150
-}, 1);
+});
 
-tl.from('.slash', {
-    x: -100,
-    stagger: 0.3
-
-}, 2);
-
-tl.to('.second_slash', {
-    x: '38vw',
-    duration: 1.4,
-    stagger: 0.4
-}, 3);
-
-
-tl.from('.inner_title', {
+tl.from('.h_top_left', {
     x: '-50vw',
-    duration: 1.4,
-    stagger: 0.8
-}, 3);
-
-tl.to('.second_slash', {
-    delay: 1.4,
-    x: -10
-}, 4);
-
-tl.from('.h3_top_left', {
-    x: '-38vw',
+    stagger: 0.8,
     duration: 1.4
-})
+});
 
 tl.from('#square_portfolio', {
     x: '25vw',
