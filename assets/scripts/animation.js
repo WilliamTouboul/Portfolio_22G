0de1cc2546d723aedea86a231c53328d2c65e268
William Touboul <Touboul.william@gmail.com>
2022-09-05 14:24:12 +0200

    debut anim

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -1,56 +1,82 @@
 /* -------------------------------------------------------------------------- */
-/*                           Animation onload header                          */
+/*                                   Loader                                   */
 /* -------------------------------------------------------------------------- */
+var tl__loader = gsap.timeline();
 
-// BLOCK SCREEN 
-function removing_lock() {
-    var element = document.querySelector('.scroll_locked');
-    element.classList.remove('scroll_locked')
+tl__loader.to('.jauge_inner', {
+    width: '100%',
+    duration: 4.5,
+    ease: 'steps(35)'
+}, 0);
+
+tl__loader.to('.loader', {
+    delay: .5,
+    duration: 1.5,
+    y: '-100vh',
+    ease: 'expo.inOut'
+}, 4);
+
+tl__loader.from('#name_top_left', {
+    y: -120,
+    opacity: 0,
+    ease: 'slow(0.5,0.7,false)'
+});
+
+tl__loader.from('.h_top_left', {
+    y: -120,
+    opacity: 0,
+    delay: .4,
+    stagger: .4,
+    ease: 'slow(0.5,0.7,false)'
+});
+
+tl__loader.from('#square_portfolio', {
+    y: -120,
+    opacity: 0,
+
+    delay: .4,
+    stagger: .4,
+    ease: 'slow(0.5,0.7,false)'
+});
+
+tl__loader.from('#main_pf_title', {
+    x: '-150%',
+    opacity: 0,
+    delay: .4,
+    ease: 'slow(0.5,0.7,false)'
+});
+
+tl__loader.from('#square_portfolio', {
+    height: 52,
+    width: 52,
+    stagger: .4,
+    ease: 'slow(0.5,0.7,false)'
+});
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
 }
 
-window.onload = function () {
-    setTimeout(() => {
-        removing_lock();
-    }, "7500")
-}
+const obj = document.querySelector('#counter')
+animateValue(obj, 0, 100, 4500);
+
+
+
+
+
 
 /* -------------------------------------------------------------------------- */
-var tl = gsap.timeline();
-
-tl.from('#name_top_left', {
-    y: 150
-});
-
-tl.from('.h_top_left', {
-    x: '-50vw',
-    stagger: 0.8,
-    duration: 1.8
-});
-
-tl.from('#square_portfolio', {
-    x: '25vw',
-    ease: "power4.out",
-    duration: 2.5
-});
-
-tl.from('#main_pf_title', {
-    x: '-45vw',
-    ease: "power4.out",
-    duration: 1.2
-});
-
-tl.from('#square_portfolio', {
-    height: 70,
-    width: 70,
-    ease: "power4.out",
-});
-
-tl.from('#main_nav', {
-    y: 48
-});
-
-
-
+/*                                Hover project                               */
+/* -------------------------------------------------------------------------- */
 let project_array = document.querySelectorAll('.list_element');
 let img = document.querySelector('.project_img');
 
